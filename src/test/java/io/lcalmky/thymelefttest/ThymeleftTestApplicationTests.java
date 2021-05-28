daf2ba80888e47251713c34069d8ec2b1cd7fc31
Jaime <lcalmsky@gmail.com>
2021-05-28 18:24:12 +0900

    Initial commit

---
+++ src/test/java/io/lcalmky/thymelefttest/ThymeleftTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lcalmky.thymelefttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleftTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
